be179fee203f62549f43e5b5e2c7e320c46e5ebb
Yuato <tonyyuan1010@gmail.com>
2024-05-21 01:01:21 -0400

    segment tree problem not solved yet

---
+++ Dynamic_Range_Minimum_Test.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int tree[400000];
+
+void build(int arr[], int l, int r){
+    
+}
+
+int main() {
+    int N, M;
+    cin>>N>>M;
+    int arr[N];
+    for (int i = 0; i<N;i++){
+        int a; cin>>a;
+        arr[i] = a;
+    }
+}
